3cb6a07c3f01af87f3728dd2c724eaa007b9af74
MoutiaFazaa <fazaamouty@gmail.com>
2020-08-18 13:01:56 -0400

    python3

---
+++ exerciceSolution.py
@@ -0,0 +1,18 @@
+#given the string:
+s =  'django'
+
+#use indexing to print out the following:
+#'d'
+s[0]
+#'o'
+s[-1]
+#'jan'
+s[1:4]
+#reverse s
+s[::-1]
+
+#using keys and indexing, grab the 'hello' from the fellowing dictionaries:
+d1 =  {'simple_key':'hello'}
+d1['simple_key']
+
+d2 =  {'k1':{'k2':'hello'}}
