666de79b4484f12c8b5e2242fa97f9e0c17355c4
MoutiaFazaa <fazaamouty@gmail.com>
2020-08-25 14:25:14 -0400

    LOOPS

---
+++ controlFlow.py
@@ -0,0 +1,10 @@
+if 1==1:
+    if 1>2:
+        print("hello")
+    elif 3==3:
+        print('elif run')
+    else:
+        print('last')
+
+        
+   
---
+++ loops.py
@@ -0,0 +1,5 @@
+
+d = {'sam':1,"frank":2,"dan":3}
+for k in d:
+    print(k)
+    print(d[k])
